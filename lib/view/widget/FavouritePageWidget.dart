5599bb1b51cf18bdf5854f0be9470250445e52ef
muhammadakbarazmi <60656961+muhammadakbarazmi@users.noreply.github.com>
2021-12-29 00:04:59 +0700

    Widget Favorite Page dan Widget Video News Page
    Widget Favorite Page dan Widget Video News Page

---
+++ lib/view/widget/FavouritePageWidget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:jadwal/view/widget/JadwalListWidget.dart';
+
+class FavouritePageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return JadwalListWidget('fav', '');
+  }
+}
