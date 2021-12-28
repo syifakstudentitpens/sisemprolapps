974ebeed173843bef5b0b785d4f70c71b33023a2
bagusjackal <faridbagus14@gmail.com>
2021-12-28 23:20:24 +0700

    main

---
+++ lib/view/widget/DashBoardWidget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:news/view/widget/JadwalListWidget.dart';
+
+class DashBoardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return JadwalListWidget('', '');
+  }
+}
